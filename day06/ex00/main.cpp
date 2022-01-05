d9702a3b0bf8b5cfd6cf544343860f363462dc0a
Yacine Hadari <yhadari@e1r2p2.1337.ma>
2022-01-05 17:13:05 +0100

    edit ex00 day06

--- day06/ex00/main.cpp
+++ day06/ex00/main.cpp
@@ -1,46 +1,46 @@
 #include <iostream>
-#include <stdlib.h>
 #include <iomanip>
 
 int main(int argc, char **argv){
 
-    argc = 0;
-    std::string str(argv[1]);
-    try{
-        int i = std::stoi(str);
-        char c = (char)i;
-        if (i >= 32)
-            std::cout << "char : '" << c << "'" << std::endl;
-        else
-            std::cout << "char : Non displayable" << std::endl;
-    }
-    catch (std::exception& e){
-        std::cout << "char : impossible" << std::endl;
-    }
+    if (argc == 2){
+        std::string str(argv[1]);
+        try{
+            int i = std::stoi(str);
+            if (i >= 32)
+                std::cout << "char : '" << static_cast<char>(i) << "'" << std::endl;
+            else
+                std::cout << "char : Non displayable" << std::endl;
+        }
+        catch (std::exception& e){
+            std::cout << "char : impossible" << std::endl;
+        }
 
-    try{
-        int i = std::stoi(str);
-        std::cout << "int : " << i << std::endl;
-    }
-    catch(std::exception& e){
-        std::cout << "int : impossible" << std::endl;
-    }
+        try{
+            int i = std::stoi(str);
+            std::cout << "int : " << i << std::endl;
+        }
+        catch(std::exception& e){
+            std::cout << "int : impossible" << std::endl;
+        }
 
-    try{
-        float f = std::stof(str);
-        std::cout << "float : " << std::fixed << std::setprecision(1) << f << "f" <<std::endl;
-    }
-    catch(std::exception& e){
-        std::cout << "float : impossible" << std::endl;
-    }
+        try{
+            float f = std::stof(str);
+            std::cout << "float : " << std::fixed << std::setprecision(1) << f << "f" <<std::endl;
+        }
+        catch(std::exception& e){
+            std::cout << "float : impossible" << std::endl;
+        }
 
-    try{
-        double d = std::stof(str);
-        std::cout << "double : " << std::fixed << std::setprecision(1) << d << std::endl;
+        try{
+            float d = std::stof(str);
+            std::cout << "double : " << std::fixed << std::setprecision(1) << static_cast<double>(d) << std::endl;
+        }
+        catch(std::exception& e){
+            std::cout << "double : impossible" << std::endl;
+        }
     }
-    catch(std::exception& e){
-        std::cout << "double : impossible" << std::endl;
-    }
-    
+    else
+        std::cout << "usage:  "<< argv[0] << " \"argument\"" << std::endl;
     return 0;
 }
