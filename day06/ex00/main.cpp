bda170b912fc6afa14b4d6eec4f3bd7cf889cce0
Yacine Hadari <yhadari@e2r8p11.1337.ma>
2022-01-07 18:49:31 +0100

    finish ex0102day06

--- day06/ex00/main.cpp
+++ day06/ex00/main.cpp
@@ -6,41 +6,57 @@ int main(int argc, char **argv){
     if (argc == 2){
         std::string str(argv[1]);
         try{
-            int i = std::stoi(str);
-            if (i >= 32)
-                std::cout << "char : '" << static_cast<char>(i) << "'" << std::endl;
-            else
-                std::cout << "char : Non displayable" << std::endl;
+            if (!std::isdigit(str[0]) && str.size() == 1)
+                std::cout << "char : '" << static_cast<char>(*argv[1]) << "'" << std::endl;
+            else{
+                int i = std::stoi(str);
+                if (i >= 32 && i <= 126)
+                    std::cout << "char : '" << static_cast<char>(i) << "'" << std::endl;
+                else
+                    std::cout << "char : Non displayable" << std::endl;
+            }
         }
         catch (std::exception& e){
             std::cout << "char : impossible" << std::endl;
         }
 
         try{
-            int i = std::stoi(str);
-            std::cout << "int : " << i << std::endl;
+            if (!std::isdigit(str[0]) && str.size() == 1)
+                std::cout << "int : " << static_cast<int>(*argv[1]) << std::endl;
+            else{
+                int i = std::stoi(str);
+                std::cout << "int : " << static_cast<int>(i) << std::endl;
+            }
         }
         catch(std::exception& e){
             std::cout << "int : impossible" << std::endl;
         }
 
         try{
-            float f = std::stof(str);
-            std::cout << "float : " << std::fixed << std::setprecision(1) << f << "f" <<std::endl;
+            if (!std::isdigit(str[0]) && str.size() == 1)
+                std::cout << "float : " << std::fixed << std::setprecision(1) << static_cast<float>(*argv[1]) << "f" <<std::endl;
+            else{
+                float f = std::stof(str);
+                std::cout << "float : " << std::fixed << std::setprecision(1) << static_cast<float>(f) << "f" <<std::endl;
+            }
         }
         catch(std::exception& e){
             std::cout << "float : impossible" << std::endl;
         }
 
         try{
-            float d = std::stof(str);
-            std::cout << "double : " << std::fixed << std::setprecision(1) << static_cast<double>(d) << std::endl;
+            if (!std::isdigit(str[0]) && str.size() == 1)
+                std::cout << "double : " << std::fixed << std::setprecision(1) << static_cast<double>(*argv[1]) << std::endl;
+            else{
+                float d = std::stof(str);
+                std::cout << "double : " << std::fixed << std::setprecision(1) << static_cast<double>(d) << std::endl;
+            }
         }
         catch(std::exception& e){
             std::cout << "double : impossible" << std::endl;
         }
     }
     else
-        std::cout << "usage:  "<< argv[0] << " \"argument\"" << std::endl;
+        std::cout << "usage:  "<< static_cast<char*>(argv[0]) << " \"argument\"" << std::endl;
     return 0;
 }
--- day06/ex01/main.cpp
+++ day06/ex01/main.cpp
@@ -24,9 +24,6 @@ class Data{
     };
 };
 
-int * test(void * i){
-    return reinterpret_cast<int*>(i);
-}
 uintptr_t   serialize(Data* ptr){
     return reinterpret_cast<uintptr_t>(ptr);
 }
@@ -39,10 +36,13 @@ int main(void){
 
     Data *objet = new Data;
     objet->SetAttribute(10);
+    std::cout << "adress 1: " << objet << "   value 1: " << objet->GetAttribute() << std::endl;
+
     uintptr_t i = serialize(objet);
     Data *objet2 = deserialize(i);
-    std::cout << "adress 1: " << objet << "   value 1: " << objet->GetAttribute() << std::endl;
+
     std::cout << "adress 2: " << objet2 << "   value 2: " << objet2->GetAttribute() << std::endl;
+    
     if (objet->GetAttribute() == objet2->GetAttribute())
         std::cout << "The Return Value Is Equal" << std::endl;
     else
